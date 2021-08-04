151cbc09c1a63c8388e2eedd80712acf79ae7bf3
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 04:40:35 +0300

    - Create input variable 'appHideHeader'.

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,9 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHideHeader]'
 })
 export class HideHeaderDirective {
+  @Input('appHideHeader') header: any;
 
   constructor() { }
 
