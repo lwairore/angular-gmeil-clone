3125248572d32afccbc528ed060ba367c081923c
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 03:48:53 +0300

    - Generate page 'Mail'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'mail',
+    loadChildren: () => import('./pages/mail/mail.module').then( m => m.MailPageModule)
   }
 ];
 
