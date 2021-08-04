969b08124bc8a79b845bcb2df8ac11fbf185dd7d
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 08:37:47 +0300

    - Create method 'setupAnimation'.

--- src/app/directives/animated-fab.directive.ts
+++ src/app/directives/animated-fab.directive.ts
@@ -8,11 +8,31 @@ export class AnimatedFabDirective implements AfterViewInit {
   @Input('appAnimatedFab') fab: any;
   expanded = true;
 
-  shrinkAnimation: Animation | undefined;
+  shrinkAnimation: any;
 
   constructor(private animationCtrl: AnimationController) { }
 
   ngAfterViewInit() {
     this.fab = this.fab.el;
+  }
+
+  setupAnimation() {
+    const textSpan = this.fab.querySelector('span');
+
+    const shrink = this.animationCtrl.create('shrink')
+      .addElement(this.fab)
+      .duration(400)
+      .fromTo('width', '140px', '50px');
+
+    const fade = this.animationCtrl.create('fade')
+      .addElement(textSpan)
+      .duration(400)
+      .fromTo('opacity', 1, 0)
+      .fromTo('width', '70px', '0px');
+
+    this.shrinkAnimation = this.animationCtrl.create('shrink-animation')
+      .duration(400)
+      .easing('ease-out')
+      .addAnimation([shrink, fade]);
   }
 }
