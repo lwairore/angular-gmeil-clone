aae4571efae55328b962fe20944fc3ada373b7ca
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 08:47:16 +0300

    - Add module 'IonicModule' to 'imports' array.

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwipeItemComponent } from './swipe-item/swipe-item.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
@@ -9,7 +10,8 @@ import { SwipeItemComponent } from './swipe-item/swipe-item.component';
     SwipeItemComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule,
   ],
   exports: [
     SwipeItemComponent,
