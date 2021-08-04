0f2c21f8e43ef5b54d1a1b26821b7c0201d30e6e
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 04:39:25 +0300

    - Document how the directive works.

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -8,3 +8,16 @@ export class HideHeaderDirective {
   constructor() { }
 
 }
+
+/**
+ ** We need to store the last Y position within `saveY`
+ * to notice in which direction we scroll
+ ** When we notice that we changed directions,
+ * we store that exact position inside `previousY`
+ * so we can use it for our caluclation
+ ** We will change the `top` and `opacity`
+ * properties of our search bar
+ ** The `scrollDistance` is the value at which
+ * the element will be gone completely,
+ * so the position is between 0 and -50, while the opacity is between 0 and 1
+ */
