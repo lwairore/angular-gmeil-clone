cec57ab5c7759036b64f8c8b5b1e6454775f7982
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 06:19:18 +0300

    - Inject 'DOCUMENT'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -14,5 +15,5 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor( @Inject(DOCUMENT) private document: Document) {}
 }
