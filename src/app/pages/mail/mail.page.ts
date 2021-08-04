8c1332931eee7943575ec7cd5333209a84638338
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 04:20:11 +0300

    - Create method 'hashCode' for creating a custom hex code.

--- src/app/pages/mail/mail.page.ts
+++ src/app/pages/mail/mail.page.ts
@@ -14,6 +14,15 @@ export class MailPage implements OnInit {
   ngOnInit() {
   }
 
+  // https://stackoverflow.com/questions/3426404/create-a-hexadecimal-colour-based-on-a-string-with-javascript
+  private hashCode(str: string) {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
   private intToRGB(i: any) {
     var c = (i & 0x00FFFFFF)
       .toString(16)
