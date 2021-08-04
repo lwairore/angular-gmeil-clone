05a35bbb073f041e218c9e1904c337d279deeebf
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 08:45:27 +0300

    - Add component 'SwipeItemComponent' to 'declarations' array.

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwipeItemComponent } from './swipe-item/swipe-item.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SwipeItemComponent,
+  ],
   imports: [
     CommonModule
   ]
