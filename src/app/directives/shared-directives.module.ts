82dbcc19f8847a9e3549372f52e6b050de247f62
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 03:52:04 +0300

    - Generate directive HideHeaderDirective

--- src/app/directives/shared-directives.module.ts
+++ src/app/directives/shared-directives.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HideHeaderDirective } from './hide-header.directive';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HideHeaderDirective
+  ],
   imports: [
     CommonModule
   ]
