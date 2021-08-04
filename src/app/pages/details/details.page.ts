999b42cee360c1b973cb13abb6d6900be6a710ba
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 05:18:38 +0300

    - Iniaitialize object 'mailDetails'.

--- src/app/pages/details/details.page.ts
+++ src/app/pages/details/details.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.page.scss'],
 })
 export class DetailsPage implements OnInit {
+  mailDetails: any;
 
   constructor() { }
 
