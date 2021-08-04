8f3a73d3d3ad38f7c079c8f75536b813a7c8453b
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 04:31:52 +0300

    - Add module 'AccountPageModule' to imports module.

--- src/app/pages/mail/mail.module.ts
+++ src/app/pages/mail/mail.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { MailPageRoutingModule } from './mail-routing.module';
 
 import { MailPage } from './mail.page';
+import { AccountPageModule } from '../account/account.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MailPageRoutingModule
+    MailPageRoutingModule,
+    AccountPageModule,
   ],
   declarations: [MailPage]
 })
