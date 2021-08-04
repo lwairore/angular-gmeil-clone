838ba068dfd15204f6fb67962b8c142154b144db
Lucas Wairore Karangu <kwairore@gmail.com>
2021-08-04 08:43:25 +0300

    - Generate component 'SwipeItemComponent'.

---
+++ src/app/components/swipe-item/swipe-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swipe-item',
+  templateUrl: './swipe-item.component.html',
+  styleUrls: ['./swipe-item.component.scss'],
+})
+export class SwipeItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
